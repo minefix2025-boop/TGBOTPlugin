18b2964f14f301134ec585f27f491e805de91ce5
minefix2025-boop <minefix2025@gmail.com>
2026-05-15 15:58:13 +0700

    Refactor TelegramConsolePlugin by removing linkCodes
    Removed unused linkCodes map and related methods.

--- src/main/java/com/example/telegramconsole/TelegramConsolePlugin.java
+++ src/main/java/com/example/telegramconsole/TelegramConsolePlugin.java
@@ -3,16 +3,11 @@ package com.example.telegramconsole;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
 
 public class TelegramConsolePlugin extends JavaPlugin {
 
     private BotManager botManager;
     private DatabaseManager databaseManager;
-    private final Map<String, UUID> linkCodes = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -22,7 +17,8 @@ public class TelegramConsolePlugin extends JavaPlugin {
         String botToken = config.getString("bot-token", "8629251193:AAErpWdzt_vNpkfhlxN8aiXlLgWkfM7h5QQ");
         long adminId = config.getLong("admin-id", 7742036100L);
 
-        databaseManager = new DatabaseManager(this);
+        // Инициализация базы JSON в папке плагина ATGCON
+        databaseManager = new DatabaseManager(getDataFolder());
 
         botManager = new BotManager(botToken, adminId, this);
         if (botManager.start()) {
@@ -54,17 +50,4 @@ public class TelegramConsolePlugin extends JavaPlugin {
 
     public BotManager getBotManager() { return botManager; }
     public DatabaseManager getDatabaseManager() { return databaseManager; }
-    public Map<String, UUID> getLinkCodes() { return linkCodes; }
-
-    public String generateLinkCode(UUID playerUUID) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder code = new StringBuilder();
-        Random rnd = new Random();
-        while (code.length() < 20) {
-            code.append(characters.charAt(rnd.nextInt(characters.length())));
-        }
-        String finalCode = code.toString();
-        linkCodes.put(finalCode, playerUUID);
-        return finalCode;
-    }
 }
