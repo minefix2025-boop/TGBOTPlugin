23faf3788d155b877b025833c782c0f1623d683a
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 14:19:07 +0700

    Update LinkCommand.java

--- src/main/java/com/minefix/tgbotplugin/LinkCommand.java
+++ src/main/java/com/minefix/tgbotplugin/LinkCommand.java
@@ -6,20 +6,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class LinkCommand implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("§cЭту команду может использовать только игрок!");
             return true;
         }
-
         Player player = (Player) sender;
         String code = String.valueOf((int) (Math.random() * 899999) + 100000);
-        
-        // Помещаем код в общую статическую память бота
         TelegramBotImpl.pendingCodes.put(code, player.getName());
-
         player.sendMessage("§⚡ §aВаш код привязки Telegram: §e§l" + code);
         return true;
     }
