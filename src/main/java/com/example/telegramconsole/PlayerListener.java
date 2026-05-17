9dda152513a78caa96eb83d501f217537af6b185
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 13:24:23 +0700

    Update PlayerListener.java

--- src/main/java/com/example/telegramconsole/PlayerListener.java
+++ src/main/java/com/example/telegramconsole/PlayerListener.java
@@ -4,33 +4,25 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerListener implements Listener {
     private final TelegramConsolePlugin plugin;
-    public PlayerListener(TelegramConsolePlugin plugin) { this.plugin = plugin; }
 
-    @EventHandler
-    public void onLogin(PlayerLoginEvent event) {
-        Player p = event.getPlayer();
-        // Используем проверку блокировки по имени
-        if (plugin.getDatabaseManager().isPlayerLocked(p.getName())) {
-            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "§c[TG] Ваш аккаунт заблокирован владельцем через Telegram или из-за подбора пароля!");
-        }
+    public PlayerListener(TelegramConsolePlugin plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
+        String name = p.getName();
         String ip = p.getAddress().getAddress().getHostAddress();
-        // Записываем IP по имени
-        plugin.getDatabaseManager().recordLoginIP(p.getName(), ip);
 
-        // Проверяем существование аккаунта по имени
-        if (!plugin.getDatabaseManager().playerExists(p.getName())) {
-            p.sendMessage("§eДобро пожаловать! Пожалуйста, зарегистрируйтесь: /reg <пароль>");
-        } else {
-            p.sendMessage("§eПожалуйста, авторизуйтесь: /login <пароль>");
+        if (plugin.getDatabaseManager().isPlayerLocked(name)) {
+            p.kickPlayer("§cВаш аккаунт временно заблокирован!");
+            return;
         }
+
+        plugin.getDatabaseManager().recordLoginIP(name, ip);
     }
 }
