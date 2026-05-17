f98ebc3f991717f539d953590d1e7e90412b4001
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 14:42:19 +0700

    Update JoinListener.java

--- src/main/java/com/minefix/tgbotplugin/JoinListener.java
+++ src/main/java/com/minefix/tgbotplugin/JoinListener.java
@@ -4,15 +4,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import java.util.UUID;
+
+// ИСПРАВЛЕНО: Добавлен недостающий импорт
+import com.minefix.tgbotplugin.DataStore;
 
 public class JoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String nick = player.getName();
-        
-        SqliteDataStore.updateLastIp(nick, player.getAddress().getAddress().getHostAddress());
-        PluginMain.getMovementBlockListener().startTimer(player);
+        UUID uuid = player.getUniqueId();
+
+        if (DataStore.isPlayerBlocked(uuid)) {
+            event.setJoinMessage(null);
+            player.kickPlayer("§cВаш аккаунт заблокирован администратором через Telegram!");
+        }
     }
 }
