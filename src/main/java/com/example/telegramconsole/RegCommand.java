dd20a9b2907f8ef51a80d3f9f4c6f70fa9736376
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 13:19:11 +0700

    Update RegCommand.java

--- src/main/java/com/example/telegramconsole/RegCommand.java
+++ src/main/java/com/example/telegramconsole/RegCommand.java
@@ -7,23 +7,34 @@ import org.bukkit.entity.Player;
 
 public class RegCommand implements CommandExecutor {
     private final TelegramConsolePlugin plugin;
-    public RegCommand(TelegramConsolePlugin plugin) { this.plugin = plugin; }
+
+    public RegCommand(TelegramConsolePlugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) return true;
         Player p = (Player) sender;
+
         if (args.length < 1) {
             p.sendMessage("§cИспользование: /reg <пароль>");
             return true;
         }
+
         if (plugin.getDatabaseManager().playerExists(p.getName())) {
-            p.sendMessage("§cВы уже зарегистрированы! Используйте /login");
+            p.sendMessage("§cВы уже зарегистрированы! Используйте /login <пароль>");
             return true;
         }
-        plugin.getDatabaseManager().registerPlayer(p.getName(), args[0], p.getUniqueId().toString());
-        plugin.getDatabaseManager().setPlayerLoggedIn(p.getName(), true);
-        p.sendMessage("§aВы успешно зарегистрировались и вошли! Свяжите аккаунт через /link");
+
+        // Регистрируем игрока в твоей базе данных
+        if (plugin.getDatabaseManager().registerPlayer(p.getName(), args[0])) {
+            p.sendMessage("§aВы успешно зарегистрировались! Защитите профиль: /link");
+            // Снимаем блокировку передвижения и удаляем из таймера напоминаний
+            plugin.getMovementBlockListener().stopTimer(p.getUniqueId());
+        } else {
+            p.sendMessage("§cПроизошла ошибка при регистрации. Попробуйте позже.");
+        }
         return true;
     }
 }
