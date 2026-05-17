3f47e2db9e0731acb0f22714b5c90b3fd8e05048
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 13:06:48 +0700

    Update DataStore.java

--- src/main/java/com/minefix/tgbotplugin/DataStore.java
+++ src/main/java/com/minefix/tgbotplugin/DataStore.java
@@ -1,22 +1,48 @@
-package com.minefix.tgbotplugin;
+package minefix.tgbotplugin;
 
 import java.util.UUID;
 
-public interface DataStore {
-    void init();
-    void close();
+public class DataStore {
 
-    boolean isRegistered(UUID uuid);
-    void savePlayerPassword(UUID uuid, String name, String bcryptHash);
-    PlayerData getPlayerData(UUID uuid);
-    void setAuthorized(UUID uuid, boolean authorized);
-    boolean isAuthorized(UUID uuid);
-    void registerFailedAttempt(UUID uuid);
+    private static SqliteDataStore sqlite;
 
-    String createLoginApproval(UUID uuid, String ip, long ttlSeconds);
-    PendingApproval getApprovalById(String id);
-    void completeApproval(String id, boolean accepted);
+    public static void init(SqliteDataStore instance) {
+        sqlite = instance;
+    }
 
-    String createLinkCode(UUID uuid, long ttlSeconds);
-    UUID consumeLinkCode(String code);
+    public static boolean isRegistered(UUID uuid) {
+        return sqlite.isRegistered(uuid);
+    }
+
+    public static boolean registerPlayer(UUID uuid, String password) {
+        return sqlite.registerPlayer(uuid, password);
+    }
+
+    public static boolean checkPassword(UUID uuid, String password) {
+        return sqlite.checkPassword(uuid, password);
+    }
+
+    public static void saveTempLinkCode(UUID uuid, String code) {
+        sqlite.saveTempLinkCode(uuid, code);
+    }
+
+    public static UUID getPlayerByLinkCode(String code) {
+        return sqlite.getPlayerByLinkCode(code);
+    }
+
+    public static void bindTelegram(UUID uuid, String chatId) {
+        sqlite.bindTelegram(uuid, chatId);
+    }
+
+    public static String getTelegramChatId(UUID uuid) {
+        return sqlite.getTelegramChatId(uuid);
+    }
+
+    public static void setPlayerBlocked(UUID uuid, boolean blocked) {
+        sqlite.setPlayerBlocked(uuid, blocked);
+    }
+
+    public static boolean isPlayerBlocked(UUID uuid) {
+        return sqlite.isPlayerBlocked(uuid);
+    }
 }
