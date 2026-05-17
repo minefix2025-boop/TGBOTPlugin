94015246af8478c11e01856f98722476f425bda8
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 14:52:54 +0700

    Update LinkCommand.java

--- src/main/java/com/minefix/tgbotplugin/LinkCommand.java
+++ src/main/java/com/minefix/tgbotplugin/LinkCommand.java
@@ -1,21 +1,34 @@
 package com.minefix.tgbotplugin;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
+
 public class LinkCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("§cЭту команду может использовать только игрок!");
+            sender.sendMessage("Команда только для игроков!");
             return true;
         }
+
         Player player = (Player) sender;
-        String code = String.valueOf((int) (Math.random() * 899999) + 100000);
-        TelegramBotImpl.pendingCodes.put(code, player.getName());
-        player.sendMessage("§⚡ §aВаш код привязки Telegram: §e§l" + code);
+        
+        String code = String.valueOf(100000 + new Random().nextInt(900000));
+        DataStore.saveTempLinkCode(player.getUniqueId(), code);
+
+        player.sendMessage("§aВаш код для привязки Telegram сгенерирован!");
+        
+        TextComponent message = new TextComponent("§eНажмите СЮДА, чтобы скопировать код: §b§l" + code);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, code));
+        
+        player.spigot().sendMessage(message);
         return true;
     }
 }
