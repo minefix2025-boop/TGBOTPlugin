88bcfcaaa9ccbd3a6c46b7dfe9555e65a291ba05
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 14:25:30 +0700

    Update DataStore.java

--- src/main/java/com/minefix/tgbotplugin/DataStore.java
+++ src/main/java/com/minefix/tgbotplugin/DataStore.java
@@ -4,45 +4,20 @@ import java.util.UUID;
 
 public class DataStore {
 
-    private static SqliteDataStore sqlite;
-
-    public static void init(SqliteDataStore instance) {
-        sqlite = instance;
-    }
-
     public static boolean isRegistered(UUID uuid) {
-        return sqlite.isRegistered(uuid);
+        return SqliteDataStore.isRegistered(uuid);
     }
 
-    public static boolean registerPlayer(UUID uuid, String password) {
-        return sqlite.registerPlayer(uuid, password);
+    public static void registerPlayer(UUID uuid, String password) {
+        SqliteDataStore.registerPlayer(uuid, password);
     }
 
     public static boolean checkPassword(UUID uuid, String password) {
-        return sqlite.checkPassword(uuid, password);
-    }
-
-    public static void saveTempLinkCode(UUID uuid, String code) {
-        sqlite.saveTempLinkCode(uuid, code);
-    }
-
-    public static UUID getPlayerByLinkCode(String code) {
-        return sqlite.getPlayerByLinkCode(code);
-    }
-
-    public static void bindTelegram(UUID uuid, String chatId) {
-        sqlite.bindTelegram(uuid, chatId);
+        return SqliteDataStore.checkPassword(uuid, password);
     }
 
     public static String getTelegramChatId(UUID uuid) {
-        return sqlite.getTelegramChatId(uuid);
-    }
-
-    public static void setPlayerBlocked(UUID uuid, boolean blocked) {
-        sqlite.setPlayerBlocked(uuid, blocked);
-    }
-
-    public static boolean isPlayerBlocked(UUID uuid) {
-        return sqlite.isPlayerBlocked(uuid);
+        Long chatId = SqliteDataStore.getChatIdByUuid(uuid);
+        return chatId != null ? String.valueOf(chatId) : null;
     }
 }
