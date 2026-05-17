3e2a301fca4b059e4eb673860b8f87eef0221f1f
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 14:41:57 +0700

    Update PluginMain.java

--- src/main/java/com/minefix/tgbotplugin/PluginMain.java
+++ src/main/java/com/minefix/tgbotplugin/PluginMain.java
@@ -1,67 +1,49 @@
 package com.minefix.tgbotplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.io.File;
+import com.example.telegramconsole.MovementBlockListener;
+import com.example.telegramconsole.TelegramConsolePlugin;
 
 public class PluginMain extends JavaPlugin {
 
-    private static PluginMain instance;
-    private TelegramBotImpl bot;
+    private static MovementBlockListener movementBlockListener;
+    private static TelegramBotImpl telegramBot;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        // Создаем папку плагина и конфиг
         if (!getDataFolder().exists()) {
             getDataFolder().mkdirs();
         }
-        saveDefaultConfig();
 
-        // Инициализируем базу данных SQLite
-        File dbFile = new File(getDataFolder(), "database.db");
-        SqliteDataStore.initialize(dbFile.getAbsolutePath());
+        // ИСПРАВЛЕНО: Передаем экземпляр плагина TelegramConsolePlugin, который ожидает конструктор
+        movementBlockListener = new MovementBlockListener(TelegramConsolePlugin.getInstance());
+        getServer().getPluginManager().registerEvents(movementBlockListener, this);
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
 
-        // Запуск Telegram Бота
+        AuthCommand authExecutor = new AuthCommand();
+        if (this.getCommand("reg") != null) this.getCommand("reg").setExecutor(authExecutor);
+        if (this.getCommand("login") != null) this.getCommand("login").setExecutor(authExecutor);
+        if (this.getCommand("link") != null) this.getCommand("link").setExecutor(new LinkCommand());
+
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            this.bot = new TelegramBotImpl(this);
-            botsApi.registerBot(this.bot);
-            getLogger().info("Telegram бот успешно запущен!");
-        } catch (TelegramApiException e) {
-            getLogger().severe("Не удалось запустить Telegram бота: " + e.getMessage());
+            telegramBot = new TelegramBotImpl(this);
+            botsApi.registerBot(telegramBot);
+            getLogger().info("Telegram-бот был успешно запущен!");
+        } catch (Exception e) {
+            getLogger().severe("Не удалось запустить Telegram бота!");
             e.printStackTrace();
         }
-
-        // Регистрация команд в Minecraft
-        getCommand("link").setExecutor(new LinkCommand());
-        if (getCommand("auth") != null) {
-            getCommand("auth").setExecutor(new AuthCommand(this));
-        }
-
-        // Регистрация ивентов блокировки движений и входа
-        getServer().getPluginManager().registerEvents(new JoinListener(), this);
-        getServer().getPluginManager().registerEvents(new MovementBlockListener(), this);
-
-        getLogger().info("TGBOTPlugin успешно включен!");
     }
 
-    @Override
-    public void onDisable() {
-        PendingApproval.clear();
-        getLogger().info("TGBOTPlugin выключен.");
+    public static MovementBlockListener getMovementBlockListener() {
+        return movementBlockListener;
     }
 
-    public static PluginMain getInstance() {
-        return instance;
-    }
-
-    public TelegramBotImpl getBot() {
-        return bot;
+    public static TelegramBotImpl getTelegramBot() {
+        return telegramBot;
     }
 }
