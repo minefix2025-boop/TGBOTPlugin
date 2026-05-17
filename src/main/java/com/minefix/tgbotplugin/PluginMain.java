190895119df388f14c00b836b452feacf533a18e
minefix2025-boop <minefix2025@gmail.com>
2026-05-17 13:09:21 +0700

    Update PluginMain.java

--- src/main/java/com/minefix/tgbotplugin/PluginMain.java
+++ src/main/java/com/minefix/tgbotplugin/PluginMain.java
@@ -1,46 +1,59 @@
-package com.minefix.tgbotplugin;
+package minefix.tgbotplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class PluginMain extends JavaPlugin {
-    private static PluginMain instance;
-    private DataStore dataStore;
-    private TelegramBotImpl telegramBot;
+
+    private static MovementBlockListener movementBlockListener;
+    private static TelegramBotImpl telegramBot;
+    private SqliteDataStore sqliteDataStore;
 
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-
-        dataStore = new SqliteDataStore(this);
-        dataStore.init();
-
-        String botToken = getConfig().getString("telegram.token", "");
-        String botUsername = getConfig().getString("telegram.username", "");
-        if (!botToken.isEmpty() && !botUsername.isEmpty()) {
-            telegramBot = new TelegramBotImpl(botToken, botUsername, dataStore, this);
-            telegramBot.start();
-        } else {
-            getLogger().warning("Telegram bot token/username не настроены в config.yml");
+        // Создаем папку плагина, если её нет
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
         }
 
-        getCommand("reg").setExecutor(new AuthCommand(dataStore));
-        getCommand("login").setExecutor(new AuthCommand(dataStore));
-        getCommand("link").setExecutor(new LinkCommand(dataStore, telegramBot));
+        // 1. Инициализация базы данных SQLite
+        this.sqliteDataStore = new SqliteDataStore(this);
+        DataStore.init(this.sqliteDataStore);
 
-        getServer().getPluginManager().registerEvents(new MovementBlockListener(dataStore), this);
-        getServer().getPluginManager().registerEvents(new JoinListener(dataStore), this);
+        // 2. Инициализация слушателей (Эвентов)
+        movementBlockListener = new MovementBlockListener(this);
+        getServer().getPluginManager().registerEvents(movementBlockListener, this);
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
 
-        getLogger().info("TGBOTPlugin включён");
+        // 3. Регистрация команд авторизации и привязки
+        AuthCommand authExecutor = new AuthCommand();
+        if (this.getCommand("reg") != null) this.getCommand("reg").setExecutor(authExecutor);
+        if (this.getCommand("login") != null) this.getCommand("login").setExecutor(authExecutor);
+        if (this.getCommand("link") != null) this.getCommand("link").setExecutor(new LinkCommand());
+
+        // 4. Запуск Telegram бота
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBot = new TelegramBotImpl(this);
+            botsApi.registerBot(telegramBot);
+            getLogger().info("Telegram-бот был успешно запущен с вашим токеном!");
+        } catch (Exception e) {
+            getLogger().severe("Не удалось запустить Telegram бота! Проверьте библиотеки и сеть.");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onDisable() {
-        if (telegramBot != null) telegramBot.stop();
-        if (dataStore != null) dataStore.close();
+        getLogger().info("Плагин TgBotPlugin выключен.");
     }
 
-    public static PluginMain getInstance() { return instance; }
-    public DataStore getDataStore() { return dataStore; }
-    public TelegramBotImpl getTelegramBot() { return telegramBot; }
+    public static MovementBlockListener getMovementBlockListener() {
+        return movementBlockListener;
+    }
+
+    public static TelegramBotImpl getTelegramBot() {
+        return telegramBot;
+    }
 }
